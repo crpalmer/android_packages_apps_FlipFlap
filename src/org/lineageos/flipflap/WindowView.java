ec6168d29e9c1c65fc9b1f378e7abf5adafa514f
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-15 15:06:00 -0500

    FlipFlap: Refactor clock / alarm into its own class for reuse
    Change-Id: I41f93a9e41ee320b9d9e7d944601ebebf80256d5

--- src/org/lineageos/flipflap/WindowView.java
+++ src/org/lineageos/flipflap/WindowView.java
@@ -40,48 +40,25 @@ public class WindowView extends RelativeLayout implements FlipFlapView {
 
     private final Context mContext;
 
-    private AlarmManager mAlarmManager;
-
     private WindowBatteryView mBatteryView;
-    private LinearLayout mClockPanel;
-
-    private TextView mHoursView;
-    private TextView mMinsView;
-    private TextView mAmPmView;
-    private TextView mDateView;
-
-    private ImageView mAlarmIcon;
-    private TextView mAlarmText;
+    private ClockPanel mClockPanel;
 
     public WindowView(Context context) {
         super(context);
 
         mContext = context;
 
-        mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
-
         inflate(mContext, R.layout.window_view, this);
-        mHoursView = (TextView) findViewById(R.id.clock1);
-        mMinsView = (TextView) findViewById(R.id.clock2);
-        mAmPmView = (TextView) findViewById(R.id.clock_ampm);
-        mDateView = (TextView) findViewById(R.id.date_regular);
-
-        mAlarmIcon = (ImageView) findViewById(R.id.alarm_icon);
-        mAlarmText = (TextView) findViewById(R.id.next_alarm_regular);
 
         mBatteryView = (WindowBatteryView) findViewById(R.id.window_battery);
 
-        mClockPanel = (LinearLayout) findViewById(R.id.clock_panel);
+        mClockPanel = (ClockPanel) findViewById(R.id.clock_panel);
         mClockPanel.bringToFront();
-
-        refreshClock();
-        refreshAlarmStatus();
     }
 
     @Override
     public void postInvalidate() {
-        refreshClock();
-        refreshAlarmStatus();
+        mClockPanel.postInvalidate();
         mBatteryView.postInvalidate();
         super.postInvalidate();
     }
@@ -99,59 +76,5 @@ public class WindowView extends RelativeLayout implements FlipFlapView {
     @Override
     public float getScreenBrightness() {
         return 0.5f;
-    }
-
-    private void refreshClock() {
-        Locale locale = Locale.getDefault();
-        Date now = new Date();
-        String dateFormat = mContext.getString(R.string.abbrev_wday_month_day_no_year);
-        CharSequence date = DateFormat.format(dateFormat, now);
-        String hours = new SimpleDateFormat(getHourFormat(), locale).format(now);
-        String minutes = new SimpleDateFormat(mContext.getString(R.string.widget_12_hours_format_no_ampm_m),
-                locale).format(now);
-        String amPm = new SimpleDateFormat(
-                mContext.getString(R.string.widget_12_hours_format_ampm), locale).format(now);
-
-        mHoursView.setText(hours);
-        mMinsView.setText(minutes);
-        mAmPmView.setText(amPm);
-        mDateView.setText(date);
-    }
-
-    private void refreshAlarmStatus() {
-        String nextAlarm = getNextAlarm();
-        if (!TextUtils.isEmpty(nextAlarm)) {
-            // An alarm is set, deal with displaying it
-            int color = mContext.getColor(R.color.clock_white);
-
-            // Overlay the selected color on the alarm icon and set the imageview
-            mAlarmIcon.setColorFilter(color);
-            mAlarmIcon.setVisibility(View.VISIBLE);
-
-            mAlarmText.setText(nextAlarm);
-            mAlarmText.setVisibility(View.VISIBLE);
-            mAlarmText.setTextColor(color);
-        } else {
-            // No alarm set or Alarm display is hidden, hide the views
-            mAlarmIcon.setVisibility(View.GONE);
-            mAlarmText.setVisibility(View.GONE);
-        }
-    }
-
-    private String getHourFormat() {
-        return DateFormat.is24HourFormat(mContext) ?
-                mContext.getString(R.string.widget_24_hours_format_h_api_16) :
-                mContext.getString(R.string.widget_12_hours_format_h);
-    }
-
-    private String getNextAlarm() {
-        AlarmManager.AlarmClockInfo nextAlarmClock = mAlarmManager.getNextAlarmClock();
-        if (nextAlarmClock != null) {
-            String skeleton = DateFormat.is24HourFormat(mContext) ? "EHm" : "Ehma";
-            String pattern = DateFormat.getBestDateTimePattern(Locale.getDefault(), skeleton);
-            return (String) DateFormat.format(pattern, nextAlarmClock.getTriggerTime());
-        }
-
-        return null;
     }
 }
